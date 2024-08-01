42cd7c59b40891911ecbbb01dd825ee00b0ed273
alejandrovelez0208 <alejandrovelezarboleda0208@outlook.com>
2024-08-01 17:17:59 -0500

    feat: ajustes

--- dominio/src/main/java/com/padre/dominio/WebConfig.java
+++ dominio/src/main/java/com/padre/dominio/WebConfig.java
@@ -1,16 +1,11 @@
 package com.padre.dominio;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-	@Autowired
-	private ApplicationContext contexto;
 
 	@Override
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
