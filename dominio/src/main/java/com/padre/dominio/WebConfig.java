f015e467cf9b9ea3eaad528e26ccb34355c5fd99
alejandrovelez0208 <alejandrovelezarboleda0208@outlook.com>
2024-08-01 17:17:44 -0500

    feat: configuracion

--- dominio/src/main/java/com/padre/dominio/WebConfig.java
+++ dominio/src/main/java/com/padre/dominio/WebConfig.java
@@ -1,29 +1,22 @@
 package com.padre.dominio;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
+public class WebConfig implements WebMvcConfigurer {
 
 	@Autowired
 	private ApplicationContext contexto;
 
 	@Override
-	public void setApplicationContext(ApplicationContext contexto) throws BeansException {
-		this.contexto = contexto;
-	}
-
-	@Override
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/componentes/**").addResourceLocations("classpath:/META-INF/frontend/componentes");
+
+		registry.addResourceHandler("/frontend/**").addResourceLocations("classpath:/frontend/views");
 	}
 
 }
